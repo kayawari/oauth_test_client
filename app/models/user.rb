345fc731ad68502c9c819d094aa601c00df56f84
takayuki.kayawari <kayamelo@gmail.com>
2020-01-04 22:43:31 +0900

    Add request to provider function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,27 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.find_for_mock(auth)
+    parameters = {
+      name: auth.info.name,
+      email: auth.info.email,
+      provider: auth.provider,
+      uid: auth.uid,
+      token: auth.credentials.token,
+      password: Devise.friendly_token[0, 20],
+      raw: auth.extra.to_json,
+      scope: 'public'
+    }
+    user = User.find_by(uid: auth.uid)
+    return update_mock!(user, parameters) if user
+
+    User.create(parameters)
+  end
+
+  def self.update_mock!(user, parameters)
+    user.update(parameters)
+    user
+  end
 end
